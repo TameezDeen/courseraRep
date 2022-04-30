12a6969de1cf87113c47195a59bb2299eb5c2496
TameezDeen <80967823+TameezDeen@users.noreply.github.com>
2022-05-01 00:04:05 +0530

    add

--- New folder/js/script.js
+++ New folder/js/script.js
@@ -1,10 +1,27 @@
-(function (window) {
-	var tameezGreeter = {};
-	tameezGreeter.name = "Tameez";
-	var greeting = "Hello "
-	tameezGreeter.sayHello = function () {
-		console.log(greeting + tameezGreeter.name);
-	}
-	window.tameezGreeter = tameezGreeter;
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.txt", 
+            function (request) {
+              var name = request.responseText;
 
-}) (window);
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello " + name + "!</h2>";
+            });
+
+        
+      });
+  }
+);
+
+
+
+
+
